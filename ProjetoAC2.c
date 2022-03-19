e14d53f65f42e2d120055ed27292f742fda308ae
lucastoll <lucasfernandestolotto@gmail.com>
2022-03-18 23:09:31 -0300

    Adornos
    Tratamentos de digitação errada e adornos com system("cls");

--- ProjetoAC2.c
+++ ProjetoAC2.c
@@ -18,15 +18,22 @@ float *multi = NULL; //Indice 0 a 6
 
 int main (){
 setlocale(LC_ALL, "portuguese");
-int i=1, grau;
-float A, B, M, ERRO, fA, fB, fM, guardaResultado, K;
-char fAfM, fBfM;
+int i=1, grau=3, situacao;
+float A, B, M, ERRO, fA, fB, fM, guardaResultado, K, aux;
+char fAfM, fBfM, controle;
 
 //Receber o grau da função entre 2 a 6
-printf("BEM VINDO a dicotomia radiotiva em C");
+do
+{
+printf("Metódo da dicotomia em C");
 printf("\n\nInforme o grau da função (2 a 6): ");
 scanf("%i", &grau);
+fflush(stdin);
+if(grau < 1.99 || grau > 6.01)
+    system("cls");
+}while(grau < 1.99 || grau > 6.01);
 
+// ALOCAÇÃO PONTEIRO
 multi = (float*)malloc(grau * sizeof(float));
 
 for(i=grau; i>=0; i--)
@@ -34,12 +41,14 @@ for(i=grau; i>=0; i--)
     if(i==grau)
         printf("\nInforme o fator multiplicador de x^%i: ", i);
     else
-    printf("Informe o fator multiplicador de x^%i: ", i);
+        printf("Informe o fator multiplicador de x^%i: ", i);
     scanf("%f", multi+i);
+    fflush(stdin);
 }
 
 //Exibindo a função recebida
-printf("\nA funcao recebida foi: ");
+system("cls");
+printf("f(x): ");
 for(i=grau; i>=0; i--)
 {
     if(i==0)
@@ -49,35 +58,77 @@ for(i=grau; i>=0; i--)
 }
 
 // RECEBE O INTERVALO
-printf("\n\nInforme o intervalo: \nA = ");
-scanf("%f", &A);
-fflush(stdin);
-printf("B = ");
-scanf("%f", &B);
-fflush(stdin);
-
-// F(A) * F(B) < 0 ?? INFORMAR OUTRO INTERVALO OU SAIR
-printf("\nVerificando se existe um 0 na função dentro do intervalo informado [f(a) * f(b) < 0] \n\n");
-
-fA = funcaoX(grau, A, 'a');
-fB = funcaoX(grau, B, 'b');
-guardaResultado = fA * fB;
-
-printf("\nf(%.2f) * f(%.2f) < 0 ??", A, B);
-printf("\n%.2f * %.2f = %.2f\n", fA, fB, guardaResultado);
-
-if(guardaResultado > 0)
+do
 {
-    printf("\nNão existe um zero na função informada dentro do intervalo [%.2f, %.2f]\n", A, B);
-    printf("Encerrando programa\n\n");
-    system("pause");
-    return 0;
-}
-else
-{
-    printf("\nExiste um zero na função informada dentro do intervalo [%.2f, %.2f]\n", A, B );
-    printf("Continuando operações...\n");
-}
+    do
+    {
+    printf("\n\nInforme o intervalo: \nA (MENOR VALOR) = ");
+    scanf("%f", &A);
+    fflush(stdin);
+    printf("B (MAIOR VALOR) = ");
+    scanf("%f", &B);
+    fflush(stdin);
+    if(B==A)
+        printf("\n\aOs valores de A e B não podem ser iguais!\n");
+    }while(!(B!=A));
+
+    // VERIFICAR VALIDADE DO INTERVALO
+    if(B<A)
+    {
+        aux = B;
+        B = A;
+        A = aux;
+        printf("\nMenor valor foi armazenado em B, as variáveis A e B serão invertidas automaticamente.\n");
+    }
+
+    // F(A) * F(B) < 0 ?? INFORMAR OUTRO INTERVALO OU SAIR
+    printf("\nVerificando se existe um 0 na função dentro do intervalo informado [f(a) * f(b) < 0] \n\n");
+
+    fA = funcaoX(grau, A, 'a');
+    fB = funcaoX(grau, B, 'b');
+    guardaResultado = fA * fB;
+
+    printf("\nf(%.2f) * f(%.2f) < 0 ??", A, B);
+    printf("\n%.2f * %.2f = %.2f\n", fA, fB, guardaResultado);
+
+    if(guardaResultado > 0)
+    {
+        printf("\nNão existe um zero na função informada dentro do intervalo [%.2f, %.2f]\n", A, B);
+        do
+        {
+        printf("\nDeseja digitar outro intervalo? <S/N>\n");
+        scanf("%c", &controle);
+        fflush(stdin);
+        }while(!(controle == 'S' || controle == 'N'));
+        if(controle == 'N')
+        {
+            printf("Encerrando programa\n\n");
+            system("pause");
+            return 0;
+        }
+        else
+        {
+            system("cls");
+            printf("FUNÇÃO: ");
+            for(i=grau; i>=0; i--)
+            {
+                if(i==0)
+                    printf("%.2f = 0", *(multi+i));
+                else
+                    printf("%.2fx^%i + ", *(multi+i), i);
+            }
+
+        }
+    }
+    else
+    {
+        printf("\nExiste um zero na função informada dentro do intervalo [%.2f, %.2f]\n", A, B );
+        printf("Continuando operações...\n");
+    }
+}while(guardaResultado > 0);
+
+
+
 // ERRO
 printf("\nInforme o erro do problema: ");
 scanf("%f", &ERRO);
@@ -88,38 +139,62 @@ K = CalculaValorK(A, B, ERRO);
 printf("\nK = %.2f (Quantas iterações serão necessárias para achar o zero da função)\n", K);
 
 // CABEÇALHO TABELA
+system("cls");
+printf("f(x): ");
+for(i=grau; i>=0; i--)
+{
+    if(i==0)
+        printf("%.2f = 0", *(multi+i));
+    else
+        printf("%.2fx^%i + ", *(multi+i), i);
+}
+printf("\n\nIntervalo [%.3f, %3.f]\nK = %.f\nErro = %.8f", A, B, K, ERRO);
 ImprimirCabecalhoTabela();
 
 // ITERAÇÃO PRINCIPAL
 i = 1;
+M = (A + B) / 2;
 while(i <= K)
 {
+    M = (A + B) / 2;
+
     fA = funcaoX(grau, A, 'a');
     fB = funcaoX(grau, B, 'b');
-    M = (A + B) / 2;
     fM = funcaoX(grau, M, 'm');
 
     if(fA * fM < 0)
         {
             fAfM = '-';
-            B = M;
+            fBfM = '+';
+            situacao = 1;
+            //B = M;
         }
     else
-        fAfM = '+';
-
-    if(fB * fM < 0)
         {
             fBfM = '-';
-            A = M;
+            fAfM = '+';
+            situacao = 2;
+            //A = M;
         }
 
+
     ImprimirTabela(i, A, B, M, fA, fB, fM, fAfM, fBfM);
+
+
+    if(situacao==1)
+    {
+        B = M;
+    }
+    else if(situacao==2)
+    {
+        A = M;
+    }
+
+
     i++;
 }
 
-
-
-
+printf("\nFim do metódo de dicotomia\n\nA raiz encontrada foi encontrada dentro do intervalo [%.3f | %.3f]\nSeu valor final foi: %.3f\n\n", A, B, M);
 system("pause");
 }
 
@@ -148,29 +223,14 @@ float CalculaValorK(float a, float b, float erro)
 
 void ImprimirCabecalhoTabela()
 {
-    printf("\n\n\t\t\tTABELA ITERAÇÕES\n\n");
-    printf("I |  a\t|  b\t|  m\t|  f(a)\t|  f(b)\t|  f(m)\t| fa*fm\t| fm*fb\t|\n");
+    printf("\n\n\t\t\t     TABELA ITERAÇÕES\n\n");
+    printf("I \t|  a\t|  b\t|  m\t|  f(a)\t|  f(b)\t|  f(m)\t| fa*fm\t| fm*fb\t|\n");
     printf("--+-----+-------+-------+-------+-------+-------+-------+-------+\n");
 }
 
 void ImprimirTabela(int iteracao, float a, float b, float m, float fA, float fB, float fM, char fAfM, char fMfB)
 {
-    printf("%i |%.2f|%.2f\t|%.2f\t|%.2f\t|%.2f\t|%.2f\t|%c\t|%c\t|\n", iteracao, a, b, m, fA, fB, fM, fAfM, fMfB);
-
+    printf("%i \t|%.3f\t|%.3f\t|%.3f\t|%.3f\t|%.3f\t|%.3f\t|%c\t|%c\t|\n", iteracao, a, b, m, fA, fB, fM, fAfM, fMfB);
 }
 
-/*
-// CALCULA PONTO MEDIO
-M = (A + B) / 2;
-printf("\nIntervalo [%.2f, %.2f]\nMédia = %.2f", A, B, M);
 
-// ERRO
-printf("Informe o erro do problema: ");
-scanf("%f", &ERRO);
-fflush(stdin);
-
-funcaoX(10000);
-
-printf("\n\n");
-system("pause");
-*/
